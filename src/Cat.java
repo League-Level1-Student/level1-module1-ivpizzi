ec3b092ed2a14c73c71af9e859c88e36626fca80
ivpizzi <League@administorsimac.attlocal.net>
2018-02-24 11:31:13 -0800

    qwertyuiopasdfghjklzxcvbnm,

--- src/Cat.java
+++ src/Cat.java
@@ -19,29 +19,41 @@ public class Cat {
 
 	public void printName() {
 		if (name == null)
-			System.out.println("i don't know my own name!");
+			System.out.println("I don't know my own name!");
 		else
-			System.out.println("my name is " + name);
+			System.out.println("My name is " + name);
 	}
 
 	void kill() {
 		lives--;
 		if (lives > 0)
-			System.out.println("nice try, but I still have " + lives + " lives left");
+			System.out.println("Nice try, but I still have " + lives + " lives left");
 		else if (lives < 0)
-			System.out.println("that's overkill yo!");
+			System.out.println("That's overkill yo!");
 		else
 			System.out.println("DEAD CAT :(");
 	}
 
 	public static void main(String[] args) {
 		/* Do the following things without changing the Cat class */
-
-		// 1. Make the Cat meow
 		
-		// 2. Get the Cat to print it's name
+		Cat cat = new Cat("Whiskers");
+		
+		// 1. Make the Cat meow
+		cat.meow();
+		
+		// 2. Get the Cat to print its name
+		cat.printName();
 
 		// 3. Kill the Cat!
+		int kills = 1;
+		while(kills <= 9)
+		{
+			
+			cat.kill();
+			kills += 1;
+			
+		}
 
 	}
 }
--- src/SeaCreature.java
+++ src/SeaCreature.java
@@ -4,7 +4,7 @@
  */
 
 /* 
- * 1. In a Runner class, make a SeaCreature called “Spongebob”. Use the methods below to make him eat, and laugh.
+ * 1. In a Runner class, make a SeaCreature called “Spongebob”. Use the methods below to make him eat and laugh.
  * 2. Make Patrick and Squidward and print their name, have them eat, and make them laugh.
  */
 
